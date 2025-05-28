434b2fcbb4aa3a0a65d79705f91d0f34375134c8
Mateo Sebastian Santos Rocha <2905037@tcs.com>
2025-05-28 15:03:13 -0500

    ingreso de una ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'productos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'productos',
+    loadComponent: () => import('./tabla-productos/tabla-productos.component').then(m => m.TablaProductosComponent)
+  }
+];
